5792de6f413edd47dab7fac4a0d8a7be3a6459f4
Hronoz87 <hromov_andrey@mail.ru>
2021-02-11 12:10:53 +0300

    Di

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -3,7 +3,6 @@ package ru.netology.servlet;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,14 +18,7 @@ public class MainServlet extends HttpServlet {
         // отдаём класс конфигурации
         final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
 
-        // получаем по имени бина
-        final var controller = context.getBean("postController");
-
-        // получаем по классу бина
-        final var service = context.getBean(PostService.class);
-
-        // по умолчанию создаётся лишь один объект на BeanDefinition
-        final var isSame = service == context.getBean("postService");
+        controller = context.getBean(PostController.class);
     }
 
     @Override
